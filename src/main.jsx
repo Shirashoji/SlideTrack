86bbcb2fbe28006292a402756bb3ae9a04fad6f7
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2023-10-20 15:30:38 +0900

    画面数を増やした

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Root from "./routes/root";
+import Upload from "./routes/upload";
+import Record from "./routes/record";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/new",
+    element: <Upload />,
+  },
+  {
+    path: "/upload",
+    element: <Upload />,
+  },
+  {
+    path: "/record/",
+    element: <Record />,
+  },
+  {
+    path: "/record/:id",
+    element: <Record />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/routes/record.jsx
@@ -0,0 +1,7 @@
+export default function Record() {
+  return (
+    <div id="main">
+      <h1>Record</h1>
+    </div>
+  );
+}
