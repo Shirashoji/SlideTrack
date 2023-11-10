163bf64d81ef2a1b66a273c7dbf1a55cae1b037a
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2023-11-10 15:38:37 +0900

    Add recording feature to speech-to-text sample

--- src/routes/speechToTextSample.jsx
+++ src/routes/speechToTextSample.jsx
@@ -1,5 +1,5 @@
 import "regenerator-runtime";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
@@ -11,10 +11,19 @@ const Dictaphone = () => {
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
+  const [records, setRecords] = useState([]);
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
+
+  useEffect(() => {
+    if (!listening && transcript !== "")
+      setRecords([
+        ...records,
+        { time: new Date(Date.now()), text: transcript },
+      ]);
+  }, [listening]);
 
   return (
     <div>
@@ -22,7 +31,16 @@ const Dictaphone = () => {
       <button onClick={SpeechRecognition.startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>speech now: {transcript}</p>
+      <p>records:</p>
+      <ul>
+        {records.map((record) => (
+          <li key={record.time}>
+            {record.time.getHours()}:{record.time.getMinutes()}:
+            {record.time.getSeconds()}: {record.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
